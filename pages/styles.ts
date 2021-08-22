b2f17205e10e51dc41c52043b4ba9da1f759da2f
Leandro <fahurleandro@gmail.com>
2021-08-21 20:52:42 -0700

    Hello Worldgit add .! Ready to start

--- pages/styles.ts
+++ pages/styles.ts
@@ -1,11 +1,6 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    min-height: 100vh;
-    padding: 0 0.5rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    width: 100%;
     height: 100vh;
 `;
--- styles/GlobalStyles.ts
+++ styles/GlobalStyles.ts
@@ -1,20 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+    :root {
+        --black: #000000;
+        --white: #ffffff;
+    }
+    
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+    
     html,
     body {
-    padding: 0;
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-        Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-    }
-
-    a {
-    color: inherit;
-    text-decoration: none;
-    }
-
-    * {
-    box-sizing: border-box;
-    }
+        max-width: 100vw;
+        max-height: 100vh;
+        width: 100%;
+        height: 100%;
+        margin: 0 auto;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    }    
 `;
